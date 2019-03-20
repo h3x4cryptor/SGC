8e3d7282ed03173c92c5609417f5251ce839f755
unknown <Ads0@Emmanuel-PC.FFPGROUP.NET>
2019-03-20 14:51:28 +0300

    fix#5

--- .smno/insert-saleman.php
+++ .smno/insert-saleman.php
@@ -2,7 +2,7 @@
 
 
 if(isset($_POST['add-saleman-button'])) {
-    include_once './db.php';
+    include 'db.php';
     $salemanname = $conn->real_escape_string($_POST['salemanname']);
     $salemanid = $conn->real_escape_string($_POST['salemanid']);
     $dep = $conn->real_escape_string($_POST['dep']);
@@ -14,23 +14,23 @@ if(isset($_POST['add-saleman-button'])) {
 
    //Error handling
     if (empty($salemanname) || empty($salemanid) ||  empty($email) || empty($pass) || empty($cpass)) {
-        header("Location: ./404.php?signup=empty");
+        header("Location: ../inc/404.php?signup=empty");
         $msg = "error..!";
         exit();
     } else {
         if (!preg_match("/^[a-zA-Z]*$/", $salemanid)) {
-            header("Location: ./404.php?add-saleman=invalid");
+            header("Location: ../inc/404.php?add-saleman=invalid");
             exit();
         } else {
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                header("Location: ./404.php?add-saleman=email-err");
+                header("Location: ../inc/404.php?add-saleman=email-err");
                 exit();
             } else {
                 $sql = "SELECT * FROM salesmen WHERE salemanid='$salemanid'";
                 $result = $conn->query($sql);
                 $resultCheck = $result->fetch_assoc();
                 if ($resultCheck->num_rows > 0) {
-                    header("Location: ./404.php?add-saleman=already-exist");
+                    header("Location: ../inc/404.php?add-saleman=already-exist");
                     exit();
                 } else {
                     
@@ -52,7 +52,7 @@ if(isset($_POST['add-saleman-button'])) {
                     $session    = session_id();
                     $time       = time();
                     $time_check = $time-600;     //We Have Set Time 5 Minutes
-                    header("Location: ./404.php?add-saleman=@🔐success");
+                    header("Location: ../inc/404.php?add-saleman=🔐success");
                     exit();
 
                         }
@@ -60,7 +60,7 @@ if(isset($_POST['add-saleman-button'])) {
                 }
             }
         } else {
-    header("Location: ./404.php?unknown-error-404");
+    header("Location: ../inc/404.php?unknown-error-404");
     exit();
 
 }
