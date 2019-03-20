0e704370ef0dafc40b0fc001fe2ebc8e4ba75372
unknown <Ads0@Emmanuel-PC.FFPGROUP.NET>
2019-03-20 13:39:34 +0300

    smenter

---
+++ .smno/redirect.php
@@ -0,0 +1,43 @@
+<?php
+if(isset($_POST['loginsmno'])) {
+    include_once 'db.php';
+    $salemanid = $conn->real_escape_string($_POST['salemanid']);
+    $pass =  $conn->real_escape_string($_POST['pass']);
+    
+
+    if (empty($salemanid) {
+        header("Location: ./index.php?login=empty");
+        $msg = "error..!";
+        exit();
+    } else {
+        $sql = "SELECT * FROM salesmen WHERE salemanid = '$salemanid'"; 
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        if ($resultCheck < 1)  {       
+            header("Location: ./index.php?login=error");
+            exit();
+        } else {
+            if ($row = mysqli_fetch_assoc($result)) {
+                $hashedPwdCheck = password_verify($pass, $row['hashedPwd']);
+                if ($hashedPwdCheck == false) {
+                header("Location: ./index.php?login=error");
+                exit();
+            } elseif ($hashedPwdCheck == true) {
+                session_start();
+                $_SESSION["salemanid"] = $row['salemanid'];
+                $_SESSION["email"] = $row['email'];
+                $logged_in = 1;
+
+                $session    = session_id();
+                $time       = time();
+                $time_check = $time-600;     //We Have Set Time 5 Minutes
+                header("Location: ./index.php?login=🔐success");
+                exit();                
+           }
+        }
+    }
+}
+} else {
+header("Location: ../inc/404.php?login=error");
+    exit();
+}
