a825760e12bb8e21abf4f3bf947352d22a945d01
Ahmed a305 <a7med@x000001.ies.com>
2019-03-20 18:44:24 +0300

    443#1

--- .smno/sm.php
+++ .smno/sm.php
@@ -13,8 +13,8 @@
   <form method="post" action="register.php">
   	<?php include('errors.php'); ?>
   	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	  <label>Saleman ID#</label>
+  	  <input type="text" name="salemanid" value="<?php echo $salemanid; ?>">
   	</div>
   	<div class="input-group">
   	  <label>Email</label>
